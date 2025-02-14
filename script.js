10948583e948339ab464522e88e8e4e65e7c3626
kashsharda <104987779+kashsharda@users.noreply.github.com>
2025-02-14 05:27:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const gif = document.getElementById("gif");
-const gifWrapper = document.getElementById("gif-wrapper");
 
 // Event listener for the "Yes" button
 yesBtn.addEventListener("click", () => {
@@ -13,9 +12,6 @@ yesBtn.addEventListener("click", () => {
     // Increase the image size
     gif.style.width = "500px";         // Change this value to increase or decrease image size
     gif.style.height = "500px";        // Change this value to increase or decrease image size
-
-    // Show the heart border
-    gifWrapper.classList.add("heart-borders");
 
     // Hide the buttons after "Yes" is clicked
     yesBtn.style.display = "none";
@@ -32,4 +28,5 @@ noBtn.addEventListener("click", () => {
     // Make the "Yes" button bigger each time "No" is clicked
     yesBtn.style.transform = "scale(" + (1 + Math.random() * 0.5) + ")";
 });
+
 
