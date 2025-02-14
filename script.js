ca93c412b97c8af990695e2bcd1600a59b251964
kashsharda <104987779+kashsharda@users.noreply.github.com>
2025-02-13 23:56:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Get references to the buttons
+// Get references to the buttons and the image
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const gif = document.getElementById("gif");
-const gifWrapper = document.getElementById("gif-wrapper");
 
 // Event listener for the "Yes" button
 yesBtn.addEventListener("click", () => {
@@ -14,21 +13,21 @@ yesBtn.addEventListener("click", () => {
     gif.style.width = "500px";         // Change this value to increase or decrease image size
     gif.style.height = "500px";        // Change this value to increase or decrease image size
 
-
     // Hide the buttons after "Yes" is clicked
     yesBtn.style.display = "none";
     noBtn.style.display = "none";
 });
 
-// Event listener for the "No" button (Make it move when hovered)
-noBtn.addEventListener("mouseenter", () => {
-    // Randomize the "No" button's position when hovered
+// Event listener for the "No" button
+noBtn.addEventListener("click", () => {
+    // Change the "No" button position randomly
     noBtn.style.position = "absolute";
     noBtn.style.top = Math.random() * 80 + "%";  // Randomize the vertical position
     noBtn.style.left = Math.random() * 80 + "%"; // Randomize the horizontal position
+
+    // Make the "Yes" button bigger each time "No" is clicked
+    yesBtn.style.transform = "scale(" + (1 + Math.random() * 0.5) + ")";
 });
 
-// Disable the "No" button from being clicked
-noBtn.setAttribute("disabled", true);
 
 
